16d81d60f6ecf457f645fd05d52f1ed14f0ce944
mjn <mjn@tripoli.local>
2020-08-20 14:55:30 -0700

    initializations

--- deepnn/initializations.py
+++ deepnn/initializations.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def initialize_parameters(n_x, n_h, n_y):
+def initialize_parameters_2L(n_x, n_h, n_y):
     """
     Argument:
     n_x -- size of the input layer
@@ -36,28 +36,81 @@ def initialize_parameters(n_x, n_h, n_y):
     return parameters
 
 
-def initialize_parameters_deep(layer_dims):
+def initialize_parameters_he(layers_dims):
     """
     Arguments:
-    layer_dims -- python array (list) containing the dimensions of each layer in our network
+    layer_dims -- python array (list) containing the size of each layer.
 
     Returns:
     parameters -- python dictionary containing your parameters "W1", "b1", ..., "WL", "bL":
-                    Wl -- weight matrix of shape (layer_dims[l], layer_dims[l-1])
-                    bl -- bias vector of shape (layer_dims[l], 1)
+                    W1 -- weight matrix of shape (layers_dims[1], layers_dims[0])
+                    b1 -- bias vector of shape (layers_dims[1], 1)
+                    ...
+                    WL -- weight matrix of shape (layers_dims[L], layers_dims[L-1])
+                    bL -- bias vector of shape (layers_dims[L], 1)
     """
 
-    np.random.seed(1)
+    #np.random.seed(3)
     parameters = {}
-    L = len(layer_dims)            # number of layers in the network
+    L = len(layers_dims) - 1  # integer representing the number of layers
+
+    for l in range(1, L + 1):
+        ### START CODE HERE ### (≈ 2 lines of code)
+        parameters['W' + str(l)] = np.random.randn(layers_dims[l], layers_dims[l - 1]) * np.sqrt(
+            2. / layers_dims[l - 1])
+        parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
+        ### END CODE HERE ###
+
+    return parameters
+
+
+def initialize_parameters_zeros(layers_dims):
+    """
+    Arguments:
+    layer_dims -- python array (list) containing the size of each layer.
+
+    Returns:
+    parameters -- python dictionary containing your parameters "W1", "b1", ..., "WL", "bL":
+                    W1 -- weight matrix of shape (layers_dims[1], layers_dims[0])
+                    b1 -- bias vector of shape (layers_dims[1], 1)
+                    ...
+                    WL -- weight matrix of shape (layers_dims[L], layers_dims[L-1])
+                    bL -- bias vector of shape (layers_dims[L], 1)
+    """
+
+    parameters = {}
+    L = len(layers_dims)  # number of layers in the network
 
     for l in range(1, L):
-        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[ l -1]) / np.sqrt \
-            (layer_dims[ l -1])  # *0.01
-        parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+        ### START CODE HERE ### (≈ 2 lines of code)
+        parameters['W' + str(l)] = np.zeros((layers_dims[l], layers_dims[l - 1]))
+        parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
+        ### END CODE HERE ###
+    return parameters
 
-        assert(parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[ l -1]))
-        assert(parameters['b' + str(l)].shape == (layer_dims[l], 1))
 
+def initialize_parameters_random(layers_dims):
+    """
+    Arguments:
+    layer_dims -- python array (list) containing the size of each layer.
+
+    Returns:
+    parameters -- python dictionary containing your parameters "W1", "b1", ..., "WL", "bL":
+                    W1 -- weight matrix of shape (layers_dims[1], layers_dims[0])
+                    b1 -- bias vector of shape (layers_dims[1], 1)
+                    ...
+                    WL -- weight matrix of shape (layers_dims[L], layers_dims[L-1])
+                    bL -- bias vector of shape (layers_dims[L], 1)
+    """
+
+    #np.random.seed(3)  # This seed makes sure your "random" numbers will be the as ours
+    parameters = {}
+    L = len(layers_dims)  # integer representing the number of layers
+
+    for l in range(1, L):
+        ### START CODE HERE ### (≈ 2 lines of code)
+        parameters['W' + str(l)] = np.random.randn(layers_dims[l], layers_dims[l - 1]) * 10.0
+        parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
+        ### END CODE HERE ###
 
     return parameters
